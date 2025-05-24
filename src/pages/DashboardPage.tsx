974b95e21fdfdf25eacd84e1e7810dd878e3454e
blopez6567 <brlopez2020@gmail.com>
2025-05-24 13:20:20 -0400

    Enhance dashboard UI with image analysis

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -52,10 +52,7 @@ const DashboardPage: React.FC = () => {
           <div className="grid grid-cols-1 gap-6">
             {viewMode === 'advanced' ? (
               <>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                  <ClashImageParser />
-                  <ModelSummary stats={currentProject.modelStats} />
-                </div>
+                <ModelSummary stats={currentProject.modelStats} />
                 
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                   <div className="lg:col-span-2">
