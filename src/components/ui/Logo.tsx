598238cd806d301b775295b671d9549a65e82966
blopez6567 <brlopez2020@gmail.com>
2025-05-24 03:10:40 -0400

    Update Logo component and Navbar

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,20 +4,22 @@ interface LogoProps {
   size?: number;
   showText?: boolean;
   className?: string;
+  logoPath?: string;
 }
 
 const Logo: React.FC<LogoProps> = ({ 
   size = 32, 
   showText = true,
-  className = ''
+  className = '',
+  logoPath
 }) => {
-  const logoPath = showText 
+  const defaultLogoPath = showText 
     ? '/images/Clash Sense Logo PNG.png'
-    : '/images/Clash Sense Logo No Text PNG No Background.png';
+    : '/images/Clash Sense Logo No Text PNG.png';
 
   return (
     <img 
-      src={logoPath}
+      src={logoPath || defaultLogoPath}
       alt="Clashsense Logo" 
       className={`h-auto ${className}`}
       style={{ width: size }}
